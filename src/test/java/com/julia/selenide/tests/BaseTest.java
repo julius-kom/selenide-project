359519757b00e28a10b8258959e6b261f00c8dde
julius-kom <131683366+julius-kom@users.noreply.github.com>
2026-09-09 13:40:00 +0300

    Add tearDown method

--- src/test/java/com/julia/selenide/tests/BaseTest.java
+++ src/test/java/com/julia/selenide/tests/BaseTest.java
@@ -5,9 +5,12 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import com.julia.selenide.config.Config;
 import com.julia.selenide.extensions.TestFailureExtension;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 @ExtendWith(TestFailureExtension.class)
 
@@ -26,5 +29,10 @@ public class BaseTest {
     @BeforeEach
     void setUp() {
         Configuration.baseUrl = Config.getBaseUrl();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        closeWebDriver();
     }
 }
